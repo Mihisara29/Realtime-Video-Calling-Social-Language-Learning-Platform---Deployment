2d8fac7c5def60e627ca71bd6db8abe139509a3f
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-07-06 13:59:47 +0530

    Preapred for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,25 +4,23 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js"
 
 const app = express();
 const PORT = process.env.PORT;
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
 
+
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -30,6 +28,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat",chatRoutes);
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+  })
+}
 
 app.listen(PORT,() => {
   console.log(`Server is running on port ${PORT}`);
